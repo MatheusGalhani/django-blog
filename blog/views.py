f90409fd5a61b7ff93f2531f6060d8e075c52a30
MatheusGalhani <matheus.galhani@hotmail.com>
2026-05-05 15:42:39 -0300

    sec: add login flow

--- blog/views.py
+++ blog/views.py
@@ -34,5 +34,32 @@ class BlogPostViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
                       mixins.UpdateModelMixin, mixins.DestroyModelMixin):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     throttle_classes = [PaginationMinuteUserRateThrottle]
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['author_id'] = request.user.id
+        serializer = self.get_serializer(
+            data=data, many=False, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        if instance.author_id != request.user.id:
+            return Response({'detail': 'You do not have permission to edit this post.'}, status=status.HTTP_403_FORBIDDEN)
+        
+        data = request.data.copy()
+        data['author_id'] = request.user.id
+        serializer = self.get_serializer(
+            instance, data=data, partial=partial, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
